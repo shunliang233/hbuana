79e2dec10b5d1cf7d54d51398a102c1633ef9584
Shunliang Zhang <shunliang.zhang@cern.ch>
2025-07-02 15:00:05 +0200

    Change setup.sh

--- config/setup.sh
+++ config/setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #source /cvmfs/sft.cern.ch/lcg/views/LCG_97rc4python3/x86_64-centos7-gcc9-opt/setup.sh
-export PATH=$PATH:PROJECTHERE/bin
+source /cvmfs/sft.cern.ch/lcg/views/LCG_105/x86_64-el9-gcc12-opt/setup.sh # on lxplus
+export PATH=PROJECTHERE/bin:$PATH
